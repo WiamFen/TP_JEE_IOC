0e70ca7b6fbeda54123c4646e8b3cd237cf92287
WiamFen <wfenjiro@gmail.com>
2026-02-27 00:10:00 +0000

    Création de la classe DaoImplV2 dans le répertoire ext

---
+++ src/main/java/ma/enset/ext/DaoImplV2.java
@@ -0,0 +1,13 @@
+package ma.enset.ext;
+
+import ma.enset.dao.IDao;
+
+public class DaoImplV2 implements IDao {
+
+    @Override
+    public double getData() {
+        System.out.println("Version capteurs ....");
+        double t=12;
+        return t;
+    }
+}
