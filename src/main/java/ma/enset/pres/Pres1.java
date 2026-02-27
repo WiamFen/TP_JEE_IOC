32d7a48be59a88b760aa7f562f563bc0eecf47d9
WiamFen <wfenjiro@gmail.com>
2026-02-27 00:26:07 +0000

    Pres1 : injection des dépendances via le constructeur avec DaoImplV2

--- src/main/java/ma/enset/pres/Pres1.java
+++ src/main/java/ma/enset/pres/Pres1.java
@@ -1,11 +1,12 @@
 package ma.enset.pres;
 
 import ma.enset.dao.DaoImpl;
+import ma.enset.ext.DaoImplV2;
 import ma.enset.metier.MetierImpl;
 
 public class Pres1 {
     public static void main(String[] args) {
-        DaoImpl d = new DaoImpl();
+        DaoImplV2 d = new DaoImplV2();
         MetierImpl metier = new MetierImpl(d);
         //metier.setDao(d); // injection des dépendances via le setter
         System.out.println("resultat = "+ metier.calcul());
