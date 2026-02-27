f218ac712f0dcc7db082ecffedaac784b7f80c75
WiamFen <wfenjiro@gmail.com>
2026-02-27 23:20:33 +0000

    Pres2 : injection des dépendances par instanciation dynamique (IDao et IMetier)

--- src/main/java/ma/enset/pres/Pres2.java
+++ src/main/java/ma/enset/pres/Pres2.java
@@ -1,18 +1,33 @@
 package ma.enset.pres;
 
 import ma.enset.dao.IDao;
+import ma.enset.metier.IMetier;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Scanner;
 
 public class Pres2 {
-    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Scanner scanner = new Scanner(new File("config.txt"));
 
         String daoClassName = scanner.nextLine();
         Class cDao = Class.forName(daoClassName);
-        IDao dao = (IDao) cDao.newInstance();
-        System.out.println(dao.getData());
+        IDao d = (IDao) cDao.newInstance();
+
+        String metierClassName = scanner.nextLine();
+        Class cMetier = Class.forName(metierClassName);
+
+        // via constructeur avec paramètres
+        IMetier metier = (IMetier) cMetier.getConstructor(IDao.class).newInstance(d);
+
+        //via setter
+        //IMetier metier = (IMetier) cMetier.getConstructor().newInstance();
+        //Method setDao = cMetier.getDeclaredMethod("setDao", IDao.class);
+        //setDao.invoke(metier,d);
+
+        System.out.println("resultat = "+ metier.calcul());
     }
 }
